cc06a210b7a03611acd977f099ddc5880ee2b24b
charleyramm-gb <charley.ramm@glassboxdigital.com>
2022-09-07 14:42:37 +0100

    /regions should return json from regions.json

--- app.ts
+++ app.ts
@@ -1,12 +1,28 @@
-import express from 'express';
+import fastify from "fastify";
+import fs from "fs";
 
-const app = express();
+const app = fastify({ logger: false });
 
-app.get('/', (req)=>{
-  req.name = 'bob';
+const getRegions = () => {
+    const data = fs.readFileSync("regions.json", "utf8");
+    return JSON.parse(data);
+}
+
+app.get('/', async (request: any, reply: any) => {
+    console.log('hit', '/');
+    return { hello: 'world' };
 });
-app.listen(3001, ()=>{
-    console.log('started');
-})
-let message: string = 'Hello, Charley!';
-console.log(message);
+
+app.get('/regions', async (request: any, reply: any) => {
+    return { regions: getRegions()};
+});
+
+app.listen(3000, (err: any, address: any) => {
+    if (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
+});
+
+
+
