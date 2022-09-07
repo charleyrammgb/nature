f606f6ceabd39348741cef22f46eea2aaaa86c6e
charleyramm-gb <charley.ramm@glassboxdigital.com>
2022-09-07 15:04:39 +0100

    /regions/:locationId should return regions filtered by locationId

--- app.ts
+++ app.ts
@@ -3,18 +3,33 @@ import fs from "fs";
 
 const app = fastify({ logger: false });
 
-const getRegions = () => {
-    const data = fs.readFileSync("regions.json", "utf8");
-    return JSON.parse(data);
-}
+const regions = [
+    {
+        "id": "1",
+        "name": "Whitlingham Broad",
+        "location": 3
+    },
+    {
+        "id": "2",
+        "name": "Mousehold Heath",
+        "location": 1
+    }
+]
 
-app.get('/', async (request: any, reply: any) => {
-    console.log('hit', '/');
-    return { hello: 'world' };
-});
+const regionIds = {
+    "North Norwich": 1,
+    "South Norwich": 2,
+    "East Norwich": 3,
+    "West Norwich": 4
+};
 
 app.get('/regions', async (request: any, reply: any) => {
-    return { regions: getRegions()};
+    return regions;
+});
+
+app.get('/regions/:locationId', async (request: any, reply: any) => {
+    const region = regions.find((region: any) => region.location === +request.params.locationId);
+    return { region };
 });
 
 app.listen(3000, (err: any, address: any) => {
